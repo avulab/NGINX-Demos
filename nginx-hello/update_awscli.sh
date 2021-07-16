50d9cd7afcb82cf898806ba3835a91dd75dc8d9d
Balakrishna Avula <balakrishna.avula@natwest.com>
2021-07-16 23:28:28 +0100

    deploy

--- nginx-hello/update_awscli.sh
+++ nginx-hello/update_awscli.sh
@@ -1,3 +1,3 @@
 curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64-2.0.30.zip" -o "awscliv2.zip"
 unzip -q awscliv2.zip
-sudo ./aws/install --bin-dir $bin_dir --install-dir /usr/local/aws --update
+sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws --update
